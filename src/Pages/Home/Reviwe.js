ad89287c7770d28e374f3a137765ffc9070c91bd
Naiemisalm <nayemislam30964@gmail.com>
2022-06-08 10:51:44 +0600

    Reviwe add

--- src/Pages/Home/Reviwe.js
+++ src/Pages/Home/Reviwe.js
@@ -3,30 +3,181 @@ import React from 'react';
 const Reviwe = () => {
     return (
         <div>
-            <h1 className='text-5-xl text-center'>Millions Business Trust Us</h1>
-            <div>
-                <div class="stats stats-vertical lg:stats-horizontal shadow gap-10">
+            <h1 className='text-center text-primary text-3xl font-bold mb-5'>Business Summary</h1>
+            <p className='text-xl mt-5'>Straight Claw Hammers are also known as framing or rip hammers. All of the Vaughan hammers are designed with attention to detail to offer you the highest quality. Computer aided design for balance and precision.</p>
 
-                    <div class="stat">
-                        <div class="stat-title">Downloads</div>
-                        <div class="stat-value">31K</div>
-                        <div class="stat-desc">Jan 1st - Feb 1st</div>
+            <div className='py-10 grid sm:grid-cols-1 md:grid-cols--2 lg:grid-cols-4 gap-5 mt-8'>
+
+                <div class="card w-76 bg-base-100 shadow-xl">
+                    <figure class="px-10 pt-10">
+                        <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/344/external-worldwide-business-and-management-kiranshastry-gradient-kiranshastry-5.png" alt="" width="80" hight="90" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <p className='text-xl font-bold'>95<span className='text-purple-500'>+ World Wide Customers</span></p>
+
                     </div>
+                </div>
+                <div class="card w-76 bg-base-100 shadow-xl">
+                    <figure class="px-10 pt-10">
+                        <img src="https://img.icons8.com/external-inipagistudio-lineal-color-inipagistudio/344/external-review-digital-marketplace-inipagistudio-lineal-color-inipagistudio.png" alt="" width="80" hight="90" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <p className='text-xl font-bold'>79<span className='text-purple-500'>+ Customers Reviews</span></p>
 
-                    <div class="stat">
-                        <div class="stat-title">New Users</div>
-                        <div class="stat-value">4,200</div>
-                        <div class="stat-desc">↗︎ 400 (22%)</div>
                     </div>
+                </div>
+                <div class="card w-76 bg-base-100 shadow-xl">
+                    <figure class="px-10 pt-10">
+                        <img src="https://i.ibb.co/dD9Zvbn/icons8-crowd-100.png" alt="" width="80" hight="90" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <p className='text-xl font-bold'>475<span className='text-purple-500'>+ Happy Clients</span> </p>
 
-                    <div class="stat">
-                        <div class="stat-title">New Registers</div>
-                        <div class="stat-value">1,200</div>
-                        <div class="stat-desc">↘︎ 90 (14%)</div>
+
                     </div>
-
+                </div>
+                <div class="card w-76 bg-base-100 shadow-xl">
+                    <figure class="px-10 pt-10">
+                        <img src="https://i.ibb.co/cxzcKYW/icons8-money-100.png" alt="" width="80" hight="90" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <p className='text-xl font-bold'>69<span className='text-purple-500'> M+ Annual revenue</span></p>
+                    </div>
                 </div>
             </div>
+
+
+            <div>
+                <h1 className='text-center text-primary text-3xl font-bold mb-5'>Reviews</h1>
+                <div className='py-10 grid sm:grid-cols-1 md:grid-cols--2 lg:grid-cols-3 gap-5 mt-8'>
+                    <div class="card 76 bg-base-100 shadow-xl image-full">
+                        <figure><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlUsaIKqhfLm1DN_gGs1KsQCJzNE9fSQTKyA&usqp=CAU" alt="Shoes" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Shakib Al Hasen</h2>
+                            <p>nice cating plus</p>
+                            <p className='text-xl'>Rating Count: <span className='font-semibold text-yellow-400'>5 Starts</span></p>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl image-full">
+                        <figure><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlUsaIKqhfLm1DN_gGs1KsQCJzNE9fSQTKyA&usqp=CAU" alt="Shoes" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Raju</h2>
+                            <p>nice cating plus</p>
+                            <p className='text-xl'>Rating Count: <span className='font-semibold text-yellow-400'>4 Starts</span></p>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl image-full">
+                        <figure><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlUsaIKqhfLm1DN_gGs1KsQCJzNE9fSQTKyA&usqp=CAU" alt="Shoes" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title"> BMC</h2>
+                            <p>nice cating plus</p>
+                            <p className='text-xl'>Rating Count: <span className='font-semibold text-yellow-400'>3 Starts</span></p>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl image-full">
+                        <figure><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlUsaIKqhfLm1DN_gGs1KsQCJzNE9fSQTKyA&usqp=CAU" alt="Shoes" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Holder</h2>
+                            <p>nice cating plus</p>
+                            <p className='text-xl'>Rating Count: <span className='font-semibold text-yellow-400'>5 Starts</span></p>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl image-full">
+                        <figure><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlUsaIKqhfLm1DN_gGs1KsQCJzNE9fSQTKyA&usqp=CAU" alt="Shoes" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Tony</h2>
+                            <p>nice cating plus</p>
+                            <p className='text-xl'>Rating Count: <span className='font-semibold text-yellow-400'>5 Starts</span></p>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl image-full">
+                        <figure><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlUsaIKqhfLm1DN_gGs1KsQCJzNE9fSQTKyA&usqp=CAU" alt="Shoes" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Tuhin</h2>
+                            <p>nice cating plus</p>
+                            <p className='text-xl'>Rating Count: <span className='font-semibold text-yellow-400'>4 Starts</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <h1 className="text-center text-primary text-3xl font-bold mb-5">Our Team Member</h1>
+                <div className='py-10 grid sm:grid-cols-1 md:grid-cols--2 lg:grid-cols-4 gap-5 mt-8'>
+                    <div class="card 76 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                            <img src="https://professional-electrician.com/wp-content/uploads/2019/05/NICEIC.jpg" alt="" height="200" class="rounded-xl" />
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Alex Smith</h2>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                            <img src="https://empire-s3-production.bobvila.com/articles/wp-content/uploads/2021/01/The-Best-Electrician-Near-Me.jpg" alt="" class="rounded-xl" />
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Smule Smith</h2>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                            <img src="https://images.unsplash.com/photo-1555963966-b7ae5404b6ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZWxlY3RyaWNpYW58ZW58MHx8MHx8&w=1000&q=80" alt="" class="rounded-xl" />
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Heary</h2>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                            <img src="https://www.urdesignmag.com/wp-content/uploads/2020/01/what-is-a-union-electrician-2.jpg" alt="" class="rounded-xl" />
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Henary</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h2 className='text-center text-primary text-3xl font-bold mb-5'>Our Partners</h2>
+                <p className='text-xl mt-5 mb-5'>While we are at the forefront of and specialize in design-build, we are very familiar with a number of delivery methods and are confident we can find the process that will best help you meet your goals.</p>
+
+                <div className='py-10 grid sm:grid-cols-1 md:grid-cols--2 lg:grid-cols-4 gap-5 mt-8'>
+                    <div class="card 76 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrUEsRA_77crcM2BYeVvarBzbxWEhY3_IYyA&usqp=CAU" alt="" height="200" class="rounded-xl" />
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title text-2xl">Black Smith</h2>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                            <img src="https://t3.ftcdn.net/jpg/01/37/40/46/360_F_137404631_ZAKA2yHAwptlGh0g5kdeLtMenG1ZPCuo.jpg" alt="" height="200" class="rounded-xl" />
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title text-2xl">Golbal Hammer</h2>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                            <img src="https://seeklogo.com/images/A/arm-hammer-logo-B32FB796EF-seeklogo.com.png" alt="" class="rounded-xl" />
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title text-2xl">ARM & Hammers</h2>
+                        </div>
+                    </div>
+                    <div class="card 76 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                            <img src="https://cdn3.vectorstock.com/i/1000x1000/09/17/electricity-logo-circle-electric-logo-icon-vector-27370917.jpg" alt="" height="200" class="rounded-xl" />
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title text-2xl">Joun Betta</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
